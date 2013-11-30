d272e0cbbcc744a54be69146392beca30967081d
ceeroover <lurk@gmx.net>
2013-11-30 14:25:25 +0100

    mindless maqetta example

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
  * This file is provided for custom JavaScript logic that your HTML files might need.
  * Maqetta includes this JavaScript file by default within HTML pages authored in Maqetta.
  */
-require(["dojo/ready"], function(ready){
+ require([	"dojo/ready",
+ 				"widgets/myChart"
+ 	], function(ready,myChart){
      ready(function(){
          // logic that requires that Dojo is fully initialized should go here
-
+    	var dat = myChart.sampledata();
+   		myChart.create("ChartContainer",dat);
      });
 });
+
